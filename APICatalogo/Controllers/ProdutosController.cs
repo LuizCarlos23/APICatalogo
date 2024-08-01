46193fad2aa31a7446c53eb1da027af592ea829b
LuizCarlos23 <lc.estevao23@gmail.com>
2024-08-01 16:17:52 -0300

    Criado o 'Controller' de Produtos

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos?.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound();
+            }
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(p => p.Id == id);
+
+            if (produto is null)
+                return NotFound("Produto não encontrado");
+
+            return produto;
+        }
+    }
+}
+
