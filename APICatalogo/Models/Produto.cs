5bcb32fa6f52eaf3c670700b4064236559e5ffb3
LuizCarlos23 <lc.estevao23@gmail.com>
2024-08-06 17:46:41 -0300

    Adicionado uma validação personalizada

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -8,15 +9,17 @@ public class Produto
 {
     public int Id { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(80, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres")]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
 
     [Required]
+    [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
     [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
 
